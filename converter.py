14853c17f563850dd38fa1d2cc6ae296917325c1
Rok Belingar <rok.belingar@gmail.com>
2020-10-15 23:39:49 +0200

    lowercase project built!

--- converter.py
+++ converter.py
@@ -1,4 +1,8 @@
 print("Hello! Welcome to mile-kilometer converter")
 distance = int(input("Please, type in the ammount of kilometers you want to convert to miles: "))
 
-print("That would ammount in " + str(distance*0.62) + " Miles")
+while distance > 0:
+    print("That would ammount in " + str(distance*0.62) + " Miles")
+    break
+
+print("Any other ammount you want to convert?")
---
+++ lowercase.py
@@ -0,0 +1,7 @@
+mood = input("Is today a beautiful day? ")
+mood.lower()
+
+if mood.lower() == "yes":
+    print("You should enjoy it :)")
+else:
+    print("Brighten up, better days are coming")
