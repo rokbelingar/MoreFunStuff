0061abbac9bddeb5378c20c7c41af86f8ef28ce6
Rok Belingar <rok.belingar@gmail.com>
2020-10-15 23:23:04 +0200

    converter built!

---
+++ converter.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to mile-kilometer converter")
+distance = int(input("Please, type in the ammount of kilometers you want to convert to miles: "))
+
+print("That would ammount in " + str(distance*0.62) + " Miles")
